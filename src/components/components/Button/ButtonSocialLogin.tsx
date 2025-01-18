801e36920a73baae73875efb4f74eb98df4c5573
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-01-18 17:56:59 -0300

    criado button de login social

---
+++ src/components/components/Button/ButtonSocialLogin.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+import googleLogo from "@/assets/googleLogo.svg";
+import { Typograph } from "@/components/components/Typograph/Typograph";
+
+interface SocialLoginButtonProps {
+  className?: string;
+}
+
+export function ButtonSocialLogin({ className }: SocialLoginButtonProps) {
+  const handleClick = () => {
+    window.location.href = "/chat";
+  };
+
+  return (
+    <Button
+      onClick={handleClick}
+      className={`flex items-center justify-center w-[287px] h-[69px] bg-white text-black border border-gray-300 hover:bg-gray-100 ${className}`}
+    >
+      <img src={googleLogo} alt="Google Logo" className="w-6 h-6" />
+      <Typograph
+        text="Entrar com o Google"
+        colorText="text-inherit"
+        variant="text2"
+        weight="medium"
+        fontFamily="poppins"
+      />
+    </Button>
+  );
+}
