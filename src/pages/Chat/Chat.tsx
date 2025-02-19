ba4d3f889707b71764589a6c190cb81c3acc044f
Igor Correa <igorsantosigor272@gmail.com>
2025-02-19 20:50:06 -0300

    acrescentando detalhes da tela de chat

--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -13,68 +13,69 @@ function WelcomeScreen() {
   };
 
   return (
-    <div className="flex min-h-screen bg-[#141414] flex-col justify-center items-center w-full max-w-full px-0 sm:px-8 md:px-16 mx-auto">
-      {/* Cabeçalho com o botão "Voltar" */}
-      <div className="absolute top-4 left-4 text-white text-lg">
+    <div className="flex min-h-screen bg-[#141414] flex-col items-center w-full max-w-full px-0 sm:px-8 md:px-16 mx-auto">
+      
+
+      <div className="bg-neutral-900 flex absolute top-0 text-lg items-center justify-between w-full px-64 py-4">
+        <button className="text-gray-200 bg-blue-800 py-2 px-10 rounded-lg text-lg font-Montserrat font-medium hover:bg-blue-900 hover:text-gray-300">
+          Voltar
+        </button>
+        
+        <button className="text-gray-200 bg-blue-800 py-2 px-10 rounded-lg text-lg font-Montserrat font-medium  hover:bg-blue-900 hover:text-gray-300">
+          Nova Conversa
+        </button>
+        
+      </div>
+
+
+      <div className="flex flex-col items-center w-full py-32">
         <Link to="/" className="flex items-center text-white text-lg space-x-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            className="bi bi-arrow-left-circle"
-            viewBox="0 0 16 16"
-          >
-            <path d="M8 16a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm-.5-11.5a.5.5 0 0 1 .5.5v4.793l-2.146-2.146a.5.5 0 0 1-.708.708l3 3a.5.5 0 0 1 .708 0l3-3a.5.5 0 0 1-.708-.708L8 9.793V5a.5.5 0 0 1-.5-.5z" />
-          </svg>
-          <span>Voltar</span>
+          <div className="mb-6 flex justify-center">
+            <img
+            src="/src/assets/logo.svg"
+            alt="Logo"
+            className="mx-auto"
+            style={{ width: "100px", height: "auto" }}
+          />
+          </div>
         </Link>
-      </div>
 
-      {/* Título */}
-      <h2 className="text-white text-5xl sm:text-6xl font-extrabold text-center mb-6">
-        Olá, Bem-vindo ao (...)
-      </h2>
+        <h2 className="text-white text-2xl sm:text-2xl font-extrabold text-center mb-6">
+          Olá, Bem-vindo ao (...)
+        </h2>
 
-      {/* Logo */}
-      <div className="mb-6 flex justify-center">
-        <img
-          src="/src/assets/logo.svg" // Caminho correto da sua logo
-          alt="Logo"
-          className="mx-auto"
-          style={{ width: "180px", height: "auto" }} // Tamanho da logo
-        />
-      </div>
-
-      {/* Texto explicativo */}
-      <p className="text-neutral-300 mb-6 text-center text-lg sm:text-xl">
-        Escolha um tema para começar
-      </p>
-
-      {/* Botões centralizados com tamanho igual */}
-      <div className="flex justify-center gap-6 mb-6 w-full max-w-lg">
+        <p className="text-neutral-300 mb-6 text-center text-lg sm:text-xl">
+          Escolha um tema para começar
+        </p>
+      
+        <div className="flex justify-center gap-6 mb-6 w-full max-w-lg">
         <Link to="/programacao" className="w-full sm:w-auto">
-          <button className="bg-[#003366] text-white py-4 px-10 rounded-lg text-lg w-full">
+          <button className="bg-blue-800 text-gray-200 font-Montserrat py-2 px-10 rounded-xl text-lg w-full">
             Programação
           </button>
         </Link>
         <Link to="/logica" className="w-full sm:w-auto">
-          <button className="bg-[#003366] text-white py-4 px-16 rounded-lg text-lg w-full">
+          <button className="bg-blue-800 text-gray-200 py-2 px-16 font-Montserrat rounded-xl text-lg w-full">
             Lógica
           </button>
         </Link>
+        </div>
+
       </div>
 
       {/* Campo de texto fixado no fundo */}
-      <div className="p-4 fixed bottom-0 left-0 right-0 bg-[#222222]">
-        <div className="flex items-center p-3 rounded-full">
+      <div className="p-4 fixed bottom-0 left-0 right-0 bg-neutral-900">
+
+        <div className="flex justify-center bg-[#1F2732] size-14 rounded-full w-full max-w-lg mx-auto">
+          <div className="flex items-center p-3 rounded-full">
           <input
             type="text"
             placeholder="Comece a escrever"
-            className="w-full bg-transparent text-white placeholder-gray-500 outline-none rounded-full py-3 px-6"
+            className="w-full bg-transparent text-gray-200 outline-none rounded-full py-3 px-6"
             value={inputText}
             onChange={handleInputChange}
           />
+
           <button onClick={handleSubmit} className="ml-2 text-blue-500">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -87,7 +88,10 @@ function WelcomeScreen() {
               <path d="M8 16a8 8 0 1 0 0-16 8 8 0 0 0 0 16zm.5-11.5a.5.5 0 0 1 .5.5v4.793l2.146-2.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 0 1 .708-.708L8 9.793V5a.5.5 0 0 1 .5-.5z" />
             </svg>
           </button>
+          
+          </div>
         </div>
+        
       </div>
     </div>
   );
