d9cd6abe20eeda578e8a65516cbbc8e9b6eb5349
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-04-12 23:53:45 -0300

    ajustes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,9 @@ import { Signup } from "@/pages/Singup/Singup";
 import { Signin } from "@/pages/Signin/Signin";
 import ProtectedRoute from "@/routes/ProtectedRoute";
 import { Chat } from "@/pages/Chat/Chat";
+import { Chart } from "@/pages/Chart/Chart";
+import { CRUD } from "@/pages/CRUD/CRUD";
+import { About } from "@/pages/About/About";
 
 export default function AppRoutes() {
   return (
@@ -13,7 +16,10 @@ export default function AppRoutes() {
       <Route path="/signup" element={<Signup />} />
 
       <Route element={<ProtectedRoute />}>
+        <Route path="/about" element={<About />} />
         <Route path="/chat" element={<Chat />} />
+        <Route path="/chart" element={<Chart />} />
+        <Route path="/crud" element={<CRUD />} />
       </Route>
     </Routes>
   );
