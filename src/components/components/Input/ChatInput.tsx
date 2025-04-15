2340a5d94175fafdfcccde74063fa6d549ef1b89
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-04-15 17:31:35 -0300

    Componentizando o projeto na tela de chat

---
+++ src/components/components/Input/ChatInput.tsx
@@ -0,0 +1,31 @@
+import { Send } from "lucide-react";
+
+interface ChatInputProps {
+    inputText: string;
+    setInputText: (text: string) => void;
+    sendMessage: (message: string) => void;
+}
+
+function ChatInput({ inputText, setInputText, sendMessage }: ChatInputProps) {
+    return (
+        <div className="fixed bottom-0 left-0 right-0 mb-8 px-4 flex justify-center">
+            <div className="flex items-center bg-[#1a1f27] size-14 rounded-full w-full max-w-lg mx-auto p-2">
+                <input
+                    type="text"
+                    placeholder="Comece a escrever"
+                    className="w-full bg-transparent text-gray-200 outline-none rounded-full py-3 px-6"
+                    value={inputText}
+                    onChange={(e) => setInputText(e.target.value)}
+                />
+                <button
+                    onClick={() => sendMessage(inputText)}
+                    className="text-neutral-200 bg-blue-700 rounded-full p-2 ml-2"
+                >
+                    <Send strokeWidth={1.5} />
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export { ChatInput };
--- src/pages/Chat/Chat.tsx
+++ src/pages/Chat/Chat.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
-import { Send, ChevronLeft, PanelRightOpen } from "lucide-react";
+import { ChevronLeft, PanelRightOpen } from "lucide-react";
 import { Header } from "@/components/components/Header/Header";
 import { useAuth } from "@/hooks/use-Auth";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 import { ChatMessages } from "../../components/components/Bot/ChatMessages";
 import { ChatResponseHandler } from "../../components/components/Bot/ChatResponseHandler";
+import {ChatInput} from "@/components/components/Input/ChatInput.tsx";
 
 function Chat() {
   const [inputText, setInputText] = useState("");
@@ -49,32 +50,24 @@ function Chat() {
         )}
       </div>
 
-      {/* mensagens */}
-      <ChatMessages messages={messages} userName={userName} />
-      {/* buttons */}
-      <ChatResponseHandler onSend={sendMessage} />
+      <ChatMessages
+          messages={messages}
+          userName={userName}
+      />
 
+      <ChatResponseHandler
+          onSend={sendMessage}
+      />
 
-      {/* input */}
-      <div className="fixed bottom-0 left-0 right-0 mb-8 px-4 flex justify-center">
-        <div className="flex items-center bg-[#1a1f27] size-14 rounded-full w-full max-w-lg mx-auto p-2">
-          <input
-            type="text"
-            placeholder="Comece a escrever"
-            className="w-full bg-transparent text-gray-200 outline-none rounded-full py-3 px-6"
-            value={inputText}
-            onChange={(e) => setInputText(e.target.value)}
-          />
-          <button
-            onClick={() => sendMessage(inputText)}
-            className="text-neutral-200 bg-blue-700 rounded-full p-2 ml-2"
-          >
-            <Send strokeWidth={1.5} />
-          </button>
-        </div>
-      </div>
+      <ChatInput
+          inputText={inputText}
+          setInputText={setInputText}
+          sendMessage={sendMessage}
+      />
 
-      <Header isOpen={menuOpen} closeMenu={() => setMenuOpen(false)} />
+      <Header
+          isOpen={menuOpen}
+          closeMenu={() => setMenuOpen(false)} />
     </div>
   );
 }
