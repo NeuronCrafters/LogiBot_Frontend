5813e1ea904cd33e9fe50ed318e3b0a6ae822296
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2025-10-16 13:04:09 -0300

    remoção do useEffect não utilizado

--- src/hooks/use-QuizFlow.tsx
+++ src/hooks/use-QuizFlow.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Question } from "@/@types/QuestionType";
 import { formatTitle } from "@/utils/formatText";
 import { ButtonData } from "@/components/components/Bot/Quiz/CategoryStep";
