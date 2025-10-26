e332ae37ff9c9c83469240a1b480e91ec7e967bd
ramoncbarbosa <ramon.cobarbosa@gmail.com>
2025-10-26 10:59:36 -0300

    feat: ajustanto vite para build

--- vite.config.ts
+++ vite.config.ts
@@ -14,28 +14,9 @@ export default defineConfig({
         rollupOptions: {
             external: [
                 'refractor/lib/core',
-                'refractor/lib/all'
+                'refractor/lib/all',
+                /^refractor\/lang\//
             ],
         },
     },
-
-  //comentar isso abaixo antes de subir para produção
-  // server: {
-  //   port: 5173,
-  //   host: '0.0.0.0',
-  //   watch: {
-  //     //garante que a atualização automática funcione bem no Docker
-  //     usePolling: true,
-  //   },
-
-  //   // proxy configurado para ser explícito sobre as rotas da API
-  //   proxy: {
-
-  //     // regra para autenticação
-  //     '/session': {
-  //       target: 'http://app:3000',
-  //       changeOrigin: true,
-  //     },
-  //   }
-  // }
 })
